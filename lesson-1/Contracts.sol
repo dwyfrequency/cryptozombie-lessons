45720257f69ccb5c863e0f73f73a7216bbcd4f1e
dwyfrequency <dwyfrequency@users.noreply.github.com>
2018-07-30 08:58:04 -0400

    adding initial contracts file

---
+++ lesson-1/Contracts.sol
@@ -0,0 +1,14 @@
+/*
+Solidity's code is encapsulated in contracts. A contract is the fundamental building block of Ethereum applications —
+all variables and functions belong to a contract, and this will be the starting point of all your projects.
+
+All solidity source code should start with a "version pragma" — a declaration of the version of the Solidity compiler this code should use. 
+This is to prevent issues with future compiler versions potentially introducing changes that would break your code.
+*/
+
+pragma solidity ^0.4.19; //1. Enter solidity version here
+
+//2. Create contract here
+contract ZombieFactory {
+
+}
